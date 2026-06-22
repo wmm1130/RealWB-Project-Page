48dfa47e7624b0fb54acc1aad9e7968c6ccec9e0
wmm1130 <zhanglin37@mail3.sysu.edu.cn>
2026-06-22 15:48:02 +0800

    Fix image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/RealWB/',
+  base: '/RealWB-Project-Page/',
   plugins: [
     vue(),
     vueDevTools(),
